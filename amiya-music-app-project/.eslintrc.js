4ddb14aebd3fc1ead9438744d8a9a64c40780f86
King-Teresia <wangxinaction@163.com>
2024-01-29 17:45:46 +0800

    添加了 eslint.js中的配置

--- amiya-music-app-project/.eslintrc.js
+++ amiya-music-app-project/.eslintrc.js
@@ -12,6 +12,9 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    // 我们加点东西
+    'indent': 0,//检查忽略掉 缩进
+    'space-before-function-paren': 0//检查忽略掉 函数参数前的意外空格;
   }
 }
